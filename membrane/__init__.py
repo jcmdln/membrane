5a3a473d766f32d0aa4f70bc363952a1f575bc78
jcmdln <jcmdln@gmail.com>
2020-10-18 16:59:29 -0400

    Sorely needed cleanup pass

---
+++ membrane/__init__.py
@@ -0,0 +1,3 @@
+from membrane.main import main
+
+main()
---
+++ membrane/main.py
@@ -0,0 +1,80 @@
+import click
+import re
+import requests
+import sys
+from typing import Any, Dict, List
+
+sources: List[str] = [
+    "http://winhelp2002.mvps.org/hosts.txt",
+    "http://www.malwaredomainlist.com/hostslist/hosts.txt",
+    "https://gitlab.com/ZeroDot1/CoinBlockerLists/raw/master/hosts",
+    "https://gitlab.com/ZeroDot1/CoinBlockerLists/raw/master/hosts_browser",
+    "https://gitlab.com/ZeroDot1/CoinBlockerLists/raw/master/hosts_optional",
+    "https://mirror.cedia.org.ec/malwaredomains/justdomains",
+    "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=1&mimetype=plaintext",
+    "https://raw.githubusercontent.com/AdAway/adaway.github.io/master/hosts.txt",
+    "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/UncheckyAds/hosts",
+    "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts",
+    "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Dead/hosts",
+    "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts",
+    "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Spam/hosts",
+    "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling-porn-social/hosts",
+    "https://raw.githubusercontent.com/lightswitch05/hosts/master/ads-and-tracking-extended.txt",
+]
+
+
+def cleanup(t: str) -> str:
+    """Cleanup a hosts list."""
+    t = re.sub(r"^\s", "", t, flags=re.MULTILINE)
+    t = re.sub(r"^#.*$", "", t, flags=re.MULTILINE)
+    t = re.sub(r"#.*$", "", t, flags=re.MULTILINE)
+    t = re.sub(r"^<.*$", "", t, flags=re.MULTILINE)
+    t = re.sub(r"^<.*$", "", t, flags=re.MULTILINE)
+    t = re.sub(r"^127.0.0.1\s", "", t, flags=re.MULTILINE)
+    t = re.sub(r"^0.0.0.0\s", "", t, flags=re.MULTILINE)
+    t = re.sub(r"^0.0.0.0$", "", t, flags=re.MULTILINE)
+    t = re.sub(r"^.*localhost$", "", t, flags=re.MULTILINE)
+    t = re.sub(r"^.*localdomain$", "", t, flags=re.MULTILINE)
+    t = re.sub(r"^.*::.*$", "", t, flags=re.MULTILINE)
+    t = re.sub(r"\r", "", t, flags=re.MULTILINE)
+    t = re.sub(r"^\s+", "", t, flags=re.MULTILINE)
+    t = re.sub(r"\s+$", "", t, flags=re.MULTILINE)
+    t = re.sub(r"\t", "", t, flags=re.MULTILINE)
+    t = re.sub(r"^$\n", "", t, flags=re.MULTILINE)
+    return t
+
+
+@click.command()
+def main() -> None:
+    domains: Dict[str, Any] = {}
+
+    print("membrane: getting sources...")
+    for source in sources:
+        r = requests.get(source).text
+        r = cleanup(r)
+
+        for line in r.split("\n"):
+            if "." in line:
+                domains[line] = ""
+
+    print("membrane: writing 'hosts.txt'...")
+    with open("hosts.txt", "w") as f:
+        for item in list(sorted(domains)):
+            f.write("0.0.0.0 %s\n" % item)
+
+    temp: Dict[str, Any] = {}
+    for item in domains:
+        item = ".".join(item.split(".")[-2:])
+        temp[item] = ""
+    domains = temp
+
+    print("membrane: writing 'domains.txt'...")
+    with open("domains.txt", "w") as f:
+        for item in list(sorted(domains)):
+            f.write("%s\n" % item)
+
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
